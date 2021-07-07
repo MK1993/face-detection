10aa8140d2f05b68b290ead8ab2752e149176863
MehrezKanzari <kanzari.1993@gmail.com>
2021-07-07 19:35:54 +0100

    Updates

--- static/js/10.2a96ef18.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfra=this.webpackJsonpfra||[]).push([[10],{388:function(e,t,c){"use strict";c.r(t);c(10);var s=c(7);t.default=function(e){var t=e.name,c=e.entries;return Object(s.jsxs)("div",{children:[" ",Object(s.jsx)("div",{className:"white f3",children:"".concat(t,", your current entry count is...")}),Object(s.jsx)("div",{className:"white f1",children:c})]})}}}]);
-//# sourceMappingURL=10.2a96ef18.chunk.js.map
